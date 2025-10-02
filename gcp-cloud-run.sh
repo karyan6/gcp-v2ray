a89ee0273b8536c60c43034eb7078de02a5222fe
karyan6 <aungkyawbk96@gmail.com>
2025-10-02 18:54:51 +0630

    Update gcp-cloud-run.sh

--- gcp-cloud-run.sh
+++ gcp-cloud-run.sh
@@ -238,7 +238,7 @@ main() {
     cleanup
     
     log "Cloning repository..."
-    if ! git clone https://github.com/nyeinkokoaung404/gcp-v2ray.git; then
+    if ! git clone https://github.com/karyan6/gcp-v2ray.git; then
         error "Failed to clone repository"
         exit 1
     fi
